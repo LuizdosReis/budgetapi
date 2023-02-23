c4a0f4f465dfa363b402cb20e4ef4b806a7054f8
Luiz dos Reis <luizdosreis@gmail.com>
2023-02-23 22:09:32 +0000

    add getAll tests

--- src/test/java/com/budgetapi/account/controller/AccountControllerTest.java
+++ src/test/java/com/budgetapi/account/controller/AccountControllerTest.java
@@ -1,10 +1,12 @@
 package com.budgetapi.account.controller;
 
 import static com.budgetapi.account.controller.AccountController.BASE_URL;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.budgetapi.account.dto.AccountDTO;
@@ -17,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.Optional;
 
 @WebMvcTest(AccountController.class)
@@ -49,5 +52,28 @@ class AccountControllerTest {
         this.mockMvc.perform(get(BASE_URL + "/"+ 1))
                 .andDo(print())
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldReturnStatusOkWhenCallGetAll() throws Exception {
+        Account account = Account.builder().id(1L).name("Nubank").currency("BRL").build();
+        AccountDTO accountDTO = new AccountDTO(account.getId(), account.getName(), account.getCurrency());
+
+        when(repository.findAll()).thenReturn(List.of(account));
+        when(mapper.toDTO(account)).thenReturn(accountDTO);
+
+        this.mockMvc.perform(get(BASE_URL))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenFindAllReturnEmpty() throws Exception {
+        when(repository.findAll()).thenReturn(List.of());
+
+        this.mockMvc.perform(get(BASE_URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",  hasSize(0)));
     }
 }
