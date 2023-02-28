5347e27c348b910ee9c12fa28c341fe484be804f
Luiz dos Reis <luizdosreis@gmail.com>
2023-02-28 20:57:04 +0000

    add missing assert

--- src/test/java/com/budgetapi/BudgetapiApplicationTests.java
+++ src/test/java/com/budgetapi/BudgetapiApplicationTests.java
@@ -3,11 +3,14 @@ package com.budgetapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BudgetapiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
